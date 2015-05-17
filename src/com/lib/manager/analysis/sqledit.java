1ae312826a9b6b6865ddc6c15abf7d97e6049543
vyouzhi <vyouzhi@localhost.localdomain>
2015-05-17 21:56:32 +0800

    sql bug

--- src/com/lib/manager/analysis/sqledit.java
+++ src/com/lib/manager/analysis/sqledit.java
@@ -76,10 +76,11 @@ public class sqledit extends Permission implements BasePerminterface {
 		setRoot("create_url", ucl.create(SliceName(stdClass)));
 		dbList();
 		String sql = porg.getKey("sql_script");
-		
-		if (sql != null ) {
+
+		if (sql != null) {
 			sql = sql.trim();
-			if(sql.length() == 0) return;
+			if (sql.length() == 0)
+				return;
 			setRoot("sql_edit", "\r\n" + sql.replace("&apos;", "\'"));
 			sql = Myreplace(sql);
 
@@ -91,29 +92,31 @@ public class sqledit extends Permission implements BasePerminterface {
 
 	@SuppressWarnings("unchecked")
 	private String tmpSql(String sql) {
-		if (sql==null) {
+		if (sql == null) {
 			return null;
 		}
 		String jsonTmp = porg.getKey("jsontmp");
-		if(jsonTmp==null || jsonTmp.length() < 1) return sql;
+		if (jsonTmp == null || jsonTmp.length() < 1)
+			return sql;
 		echo(jsonTmp);
 		List<List<String>> jsonList = JSON.parseObject(jsonTmp, List.class);
-		if(jsonList == null) return sql;
+		if (jsonList == null)
+			return sql;
 		List<String> tList = new ArrayList<>();
 		for (List<String> list : jsonList) {
 			tList.add(list.get(0));
-			sql = sql.replace("@"+list.get(0)+"@", list.get(1));
+			sql = sql.replace("@" + list.get(0) + "@", list.get(1));
 		}
-		
+
 		setRoot("temples", JSON.toJSONString(tList));
 		setRoot("jsonList", jsonList);
-		
+
 		return sql;
 	}
 
 	private void SqlView(String o) {
 		String sql = o;
-		List<Map<String, Object>> res;
+		List<Map<String, Object>> res = null;
 
 		sql = sql.replace("\r", " ");
 		sql = sql.replace("\t", " ");
@@ -124,26 +127,28 @@ public class sqledit extends Permission implements BasePerminterface {
 		if (sql.toLowerCase().matches("(.*)limit(.*)") == false) {
 			sql += " LIMIT 20";
 		}
-		if(porg.getKey("sql_type").equals("1")){
+		if (porg.getKey("sql_type").equals("1")) {
 			sql = tmpSql(sql);
 			setRoot("sql_type", porg.getKey("sql_type"));
 		}
-		
-		try {
-			if (dbid == 0) {
+		if (dbid == 0) {
+			try {
 				res = FetchAll(sql);
-			} else {
-				res = CustomDB(sql, dbid);
-
+			} catch (SQLException e) {
+				setRoot("ErrorMsg", e.getMessage().toString());
+			} catch (ArrayIndexOutOfBoundsException e) {
+				// TODO: handle exception
+				setRoot("ErrorMsg", "error sql");
 			}
-			if (res != null && res.size() > 0) {
-				Set<String> key = res.get(0).keySet();
+		} else {
+			res = CustomDB(sql, dbid);
 
-				setRoot("Key_Title", key);
-				setRoot("List_Data", res);
-			}
-		} catch (SQLException e) {
-			setRoot("ErrorMsg", e.getMessage().toString());
+		}
+		if (res != null && res.size() > 0) {
+			Set<String> key = res.get(0).keySet();
+
+			setRoot("Key_Title", key);
+			setRoot("List_Data", res);
 		}
 	}
 
@@ -216,42 +221,45 @@ public class sqledit extends Permission implements BasePerminterface {
 		String usql = porg.getKey("sql");
 		String jsonTmp = porg.getKey("jsontmp");
 		String nview = porg.getKey("nview");
-		
-		int is_get_data = toInt(porg.getKey("get_data"));
-				
-		int sql_type = toInt(porg.getKey("sql_type"));
-				
-		int save_id = toInt(porg.getKey("save_id"));
-		
 
-		if (name == null || usql == null )
+		int is_get_data = toInt(porg.getKey("get_data"));
+
+		int sql_type = toInt(porg.getKey("sql_type"));
+
+		int save_id = toInt(porg.getKey("save_id"));
+
+		if (name == null || usql == null)
 			return;
-				
+
 		usql = usql.replace("\r", " ");
 		usql = usql.replace("\t", " ");
 		usql = usql.replace("\n", " ");
 		usql = usql.replace("'", "&apos;");
 		// echo(usql);
 
-		if(jsonTmp == null) jsonTmp = "";
-		if(nview == null) nview = "";
-		
-		String format = " insert INTO " + DB_HOR_PRE
+		if (jsonTmp == null)
+			jsonTmp = "";
+		if (nview == null)
+			nview = "";
+
+		String format = " insert INTO "
+				+ DB_HOR_PRE
 				+ "usersql (name,sql, dtype, sql_type, sqltmp, input_data, uview)values('%s','%s', %d, %d, '%s', %d, '%s');";
-		String sql = String.format(format, name, usql, save_id, sql_type, jsonTmp, is_get_data, nview);
+		String sql = String.format(format, name, usql, save_id, sql_type,
+				jsonTmp, is_get_data, nview);
 		echo(sql);
-		
+
 		UrlClassList ucl = UrlClassList.getInstance();
 		String msg = _CLang("ok_save");
 		try {
 			insert(sql);
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			//e.printStackTrace();
+			// e.printStackTrace();
 			msg = e.getMessage();
 		}
-		
+
 		TipMessage(ucl.read(SliceName(stdClass)), msg);
 	}
 
