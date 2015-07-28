6ee4fb59b58cdbf8c9c4195edce056d16e5487a0
tea <vyouzhi@gmail.com>
2015-07-28 18:11:57 +0800

    display

---
+++ src/com/lang/manager/dashboard/_SaveDoc.java
@@ -0,0 +1,12 @@
+package com.lang.manager.dashboard;
+
+import org.ppl.BaseClass.LibLang;
+
+public class _SaveDoc extends LibLang{
+	public _SaveDoc() {
+		// TODO Auto-generated constructor stub
+		String className = this.getClass().getCanonicalName();
+		GetSubClassName(className);
+		SelfPath(this.getClass().getPackage().getName());
+	}
+}
---
+++ src/com/lang/manager/dashboard/_SaveImg.java
@@ -0,0 +1,12 @@
+package com.lang.manager.dashboard;
+
+import org.ppl.BaseClass.LibLang;
+
+public class _SaveImg extends LibLang{
+	public _SaveImg() {
+		// TODO Auto-generated constructor stub
+		String className = this.getClass().getCanonicalName();
+		GetSubClassName(className);
+		SelfPath(this.getClass().getPackage().getName());
+	}
+}
