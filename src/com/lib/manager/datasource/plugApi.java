bf4f83671649b469c6493df1d79b5f7f714e3a47
tea <vyouzhi@gmail.com>
2015-11-21 17:59:50 +0800

    plugapi

--- src/com/lib/manager/datasource/plugApi.java
+++ src/com/lib/manager/datasource/plugApi.java
@@ -67,7 +67,7 @@ public class plugApi extends Permission implements BasePerminterface {
 		UrlClassList ucl = UrlClassList.getInstance();
 		String pack = porg.getKey("pack");
 		//TellPostMan(pack, null);
-		for (int i = 4; i < 7; i++) {			
+		for (int i = 7; i < 11; i++) {			
 			TellPostMan(pack, String.format("%02d", i));
 		}
 		//TellPostMan(pack, "99");
